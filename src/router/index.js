f5ef4bfffdff486047450c6f0c18a6cf95178fe9
Sahiba Mittal <sahiba.mittal@citi.com>
2024-08-01 14:33:18 +0100

    Add tag management view
    Co-Authored-By: Niklas <nscuro@protonmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const DefaultContainer = () => import('@/containers/DefaultContainer');
 // Views
 const Dashboard = () => import('@/views/Dashboard');
 const ProjectList = () => import('@/views/portfolio/projects/ProjectList');
+const TagList = () => import('@/views/portfolio/tags/TagList.vue');
 const ComponentSearch = () =>
   import('@/views/portfolio/components/ComponentSearch');
 const VulnerabilityList = () =>
@@ -264,6 +265,17 @@ function configRoutes() {
             i18n: 'message.vulnerabilities',
             sectionPath: '/vulnerabilities',
             permissions: ['VIEW_PORTFOLIO'],
+          },
+        },
+        {
+          path: 'tags',
+          name: 'Tags',
+          component: TagList,
+          meta: {
+            title: i18n.t('message.tags'),
+            i18n: 'message.tags',
+            sectionPath: '/tags',
+            permission: 'VIEW_PORTFOLIO',
           },
         },
         {
