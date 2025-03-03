5bd07f81a6f60de825deef64ff45ae6104aae38b
Ephraim Mensah <ephraim.e.mensah@lmco.com>
2025-03-03 23:29:49 +0000

    feat: add ldapUser with Role & Project Membership
    Signed-off-by: Ephraim Mensah <ephraim.e.mensah@lmco.com>

--- src/views/administration/accessmanagement/LdapUsers.vue
+++ src/views/administration/accessmanagement/LdapUsers.vue
@@ -19,6 +19,7 @@
       </bootstrap-table>
     </b-card-body>
     <create-ldap-user-modal v-on:refreshTable="refreshTable" />
+    <select-project-modal v-on:selection="updateProjectSelection" />
   </b-card>
 </template>
 
@@ -33,6 +34,8 @@ import ActionableListGroupItem from '../../components/ActionableListGroupItem';
 import SelectTeamModal from './SelectTeamModal';
 import SelectPermissionModal from './SelectPermissionModal';
 import permissionsMixin from '../../../mixins/permissionsMixin';
+import SelectRoleModal from './SelectRoleModal.vue';
+import SelectProjectModal from './SelectProjectModal.vue';
 
 export default {
   props: {
@@ -41,6 +44,7 @@ export default {
   mixins: [bootstrapTableMixin],
   components: {
     CreateLdapUserModal,
+    SelectProjectModal,
   },
   mounted() {
     EventBus.$on('admin:ldapusers:rowUpdate', (index, row) => {
@@ -86,6 +90,18 @@ export default {
               : 0;
           },
         },
+        {
+          title: this.$t('admin.roles'),
+          field: 'roles',
+          sortable: false,
+          formatter(value, row, index) {
+            return value
+              ? xssFilters.inHTMLData(
+                  common.valueWithDefault(value.length, '0'),
+                )
+              : 0;
+          },
+        },
       ],
       data: [],
       options: {
@@ -118,6 +134,7 @@ export default {
                         <actionable-list-group-item :add-icon="true" v-on:actionClicked="$root.$emit('bv::show::modal', 'selectTeamModal')"/>
                       </div>
                     </b-form-group>
+                    </b-form-group>
                     <b-form-group :label="this.$t('admin.permissions')">
                       <div class="list-group">
                         <span v-for="permission in permissions">
@@ -126,21 +143,37 @@ export default {
                         <actionable-list-group-item :add-icon="true" v-on:actionClicked="$root.$emit('bv::show::modal', 'selectPermissionModal')"/>
                       </div>
                     </b-form-group>
+                    <b-form-group :label="this.$t('admin.roles')">
+                      <div class="list-group">
+                        <span v-for="role in roles">
+                          <actionable-list-group-item :value="role.name" :delete-icon="true" v-on:actionClicked="removeRoleMembership(role.uuid)"/>
+                          <span v-for="project in role.projects">
+                            {{ project.name }}
+                          </span>
+                          <b-button size="sm" variant="outline-primary" @click="selectProjectForRole(role)">{{ $t('admin.select_project') }}</b-button>
+                        </span>
+                        <actionable-list-group-item :add-icon="true" v-on:actionClicked="$root.$emit('bv::show::modal', 'selectRoleModal')"/>
+                      </div>
+                    </b-form-group>
                   </b-col>
-                  <b-col sm="6">
+                 </b-col>
                     <div style="text-align:right">
                        <b-button variant="outline-danger" @click="deleteUser">{{ $t('admin.delete_user') }}</b-button>
                     </div>
                   </b-col>
+                  <select-role-modal v-on:selection="updateRoleSelection" />
                   <select-team-modal v-on:selection="updateTeamSelection" />
                   <select-permission-modal v-on:selection="updatePermissionSelection" />
+                  <select-project-modal v-on:selection="updateProjectSelection" />
                 </b-row>
               `,
             mixins: [permissionsMixin],
             components: {
               ActionableListGroupItem,
+              SelectRoleModal,
               SelectTeamModal,
               SelectPermissionModal,
+              SelectProjectModal,
             },
             data() {
               return {
@@ -148,6 +181,8 @@ export default {
                 username: row.username,
                 teams: row.teams,
                 permissions: row.permissions,
+                projects: row.projects,
+                roles: row.roles,
               };
             },
             methods: {
@@ -213,6 +248,103 @@ export default {
                     this.$toastr.w(this.$t('condition.unsuccessful_action'));
                   });
               },
+              updateRoleSelection: function (selections) {
+                this.$root.$emit('bv::hide::modal', 'selectRoleModal');
+                for (let i = 0; i < selections.length; i++) {
+                  let selection = selections[i];
+                  let url = `${this.$api.BASE_URL}/${this.$api.URL_USER}/${this.username}/role`;
+                  this.axios
+                    .post(url, {
+                      uuid: selection.uuid,
+                    })
+                    .then((response) => {
+                      this.syncVariables(response.data);
+                      EventBus.$emit(
+                        'admin:ldapusers:rowUpdate',
+                        index,
+                        this.ldapUser,
+                      );
+                      this.$toastr.s(this.$t('message.updated'));
+                    })
+                    .catch((error) => {
+                      if (error.response.status === 304) {
+                        //this.$toastr.w(this.$t('condition.unsuccessful_action'));
+                      } else {
+                        this.$toastr.w(
+                          this.$t('condition.unsuccessful_action'),
+                        );
+                      }
+                    });
+                }
+              },
+              updateProjectSelection: function (selections) {
+                this.$root.$emit('bv::hide::modal', 'selectProjectModal');
+                for (let i = 0; i < selections.length; i++) {
+                  let selection = selections[i];
+                  let url = `${this.$api.BASE_URL}/${this.$api.URL_USER}/${this.username}/project`;
+                  this.axios
+                    .post(url, {
+                      uuid: selection.uuid,
+                    })
+                    .then((response) => {
+                      this.syncVariables(response.data);
+                      EventBus.$emit(
+                        'admin:ldapusers:rowUpdate',
+                        index,
+                        this.ldapUser,
+                      );
+                      this.$toastr.s(this.$t('message.updated'));
+                    })
+                    .catch((error) => {
+                      if (error.response.status === 304) {
+                        //this.$toastr.w(this.$t('condition.unsuccessful_action'));
+                      } else {
+                        this.$toastr.w(
+                          this.$t('condition.unsuccessful_action'),
+                        );
+                      }
+                    });
+                }
+              },
+              removeProjectMembership: function (projectUuid) {
+                let url = `${this.$api.BASE_URL}/${this.$api.URL_USER}/${this.username}/project`;
+                this.axios
+                  .delete(url, { data: { uuid: projectUuid } })
+                  .then((response) => {
+                    this.syncVariables(response.data);
+                    EventBus.$emit(
+                      'admin:ldapusers:rowUpdate',
+                      index,
+                      this.ldapUser,
+                    );
+                    this.$toastr.s(this.$t('message.updated'));
+                  })
+                  .catch((error) => {
+                    this.$toastr.w(this.$t('condition.unsuccessful_action'));
+                  });
+              },
+              removeRole: function (role) {
+                let url = `${this.$api.BASE_URL}/${this.$api.URL_USER}/${this.username}/role`;
+                this.axios
+                  .delete(url, {
+                    data: {
+                      roleName: role.name,
+                      projectName: role.projectName,
+                    },
+                  })
+                  .then((response) => {
+                    this.syncVariables(response.data);
+                    EventBus.$emit(
+                      'admin:ldapusers:rowUpdate',
+                      index,
+                      this.ldapUser,
+                    );
+                    this.$toastr.s(this.$t('message.updated'));
+                  })
+                  .catch((error) => {
+                    this.$toastr.w(this.$t('condition.unsuccessful_action'));
+                  });
+              },
               updatePermissionSelection: function (selections) {
                 this.$root.$emit('bv::hide::modal', 'selectPermissionModal');
                 for (let i = 0; i < selections.length; i++) {
@@ -251,6 +383,7 @@ export default {
               syncVariables: function (ldapUser) {
                 this.ldapUser = ldapUser;
                 this.username = ldapUser.username;
+                this.roles = ldapUser.roles;
                 this.teams = ldapUser.teams;
                 this.permissions = ldapUser.permissions;
               },
